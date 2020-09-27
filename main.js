ccb527a39f8087be29e2794b018af9b5f8c17365
hiroto0908 <71715048+hiroto0908@users.noreply.github.com>
2020-09-27 19:15:23 +0900

    完了ボタン追加

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var day = date.getDate();
 
 //今日の日付を表示
 document.getElementById("today").innerHTML =
-  year + "年" + month + "月" + day + "日のTODOリスト";
+  year + "年" + month + "月" + day + "日のTodoリスト";
 
 const addTaskTrigger = document.getElementsByClassName('js-addTask-trigger')[0];
 const addTaskTarget = document.getElementsByClassName('js-addTask-target')[0];
@@ -19,14 +19,28 @@ const removeTask = removeButton => {
 const addTask = task => {
   const listItem = document.createElement('li');
   const removeButton = document.createElement('button');
+  const completeButton = document.createElement('button');
   removeButton.innerText = '削除';
   removeButton.addEventListener('click', () => removeTask(removeButton));
   listItem.innerText = task;
   listItem.append(removeButton);
   addTaskTarget.appendChild(listItem);
+  completeButton.innerText = '完了';
+  completeButton.addEventListener('click', () => completeTask(completeButton));
+  listItem.append(completeButton);
+  listItem.append(removeButton);
+  addTaskTarget.appendChild(listItem);
 };
+const completeTask = completeButton => {
+  const targetTask = completeButton.closest('li');
+  targetTask.classList.add('isComplete');
+  targetTask.removeChild(completeButton);
+};
+
 addTaskTrigger.addEventListener('click', event => {
   const task = addTaskValue.value;
   addTask(task);
   addTaskValue.value = '';
 });
+
+
